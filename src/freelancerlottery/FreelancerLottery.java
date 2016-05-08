0d98a78e68c725b50c8ea3ccb208e35924a99c5f
Dang Duong Hung <hanbdi@gmail.com>
2016-05-09 00:35:35 +0800

    initilize

---
+++ src/freelancerlottery/FreelancerLottery.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freelancerlottery;
+
+/**
+ *
+ * @author duonghung1269
+ */
+public class FreelancerLottery {
+    int i1, i2 = 9;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FreelancerLottery f = new FreelancerLottery();
+        System.out.print(f.i1);
+        System.out.print(f.i2);
+    }
+    
+}
